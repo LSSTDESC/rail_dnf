29d02d6c242001c422fe6b8efcf0c423b19eccfa
ltoribiosc <lauratoribiosc@gmail.com>
2023-11-13 14:45:58 +0100

    add function of dnf.py

--- src/rail/estimation/algos/dnf.py
+++ src/rail/estimation/algos/dnf.py
@@ -21,3 +21,20 @@ def meaning() -> int:
         The meaning of life.
     """
     return 42
+    
+    
+class DNFInformer(CatInformer):
+    """Descripcion of the funcion ***
+    """
+    name = 'DNFInformer'
+#    config_options = CatInformer.config_options.copy()
+#    config_options.update(bands=SHARED_PARAMS,
+#                          err_bands=SHARED_PARAMS,
+#                          redshift_col=SHARED_PARAMS,
+#                          mag_limits=SHARED_PARAMS,
+#                          nondetect_val=SHARED_PARAMS,
+#                          nondetect_replace=Param(bool, False, msg="set to True to replace non-detects,"
+#                                                  " False to ignore in distance calculation"))
+
+
+#class DNFEstimator(CatEstimator):
