de3e1b907b508bd2a8569e4a5e5cc20d6364058a
sschmidt23 <sschmidt@physics.ucdavis.edu>
2025-01-13 14:24:45 -0800

    add tests of selection modes

--- tests/test_algos.py
+++ tests/test_algos.py
@@ -1,11 +1,10 @@
-import numpy as np
 import pytest
 # from rail.core.stage import RailStage
 from rail.utils.testing_utils import one_algo
 from rail.estimation.algos import dnf
 
 
-def test_dnf():
+def test_dnf_ANF():
     train_config_dict = {'zmin': 0.0, 'zmax': 3.0, 'nzbins': 301,
                          'hdf5_groupname': 'photometry',
                          'model': 'model.tmp'}
@@ -18,6 +17,74 @@ def test_dnf():
 
     train_algo = dnf.DNFInformer
     pz_algo = dnf.DNFEstimator
-    results, rerun_results, _ = one_algo("DNF", train_algo, pz_algo, train_config_dict, estim_config_dict)
-    #assert np.isclose(results.ancil['mode'], rerun_results.ancil['mode']).all()
-    #assert np.isclose(results.ancil['mean'], rerun_results.ancil['mean']).all()
+    results, rerun_results, _ = one_algo("DNF_ANF", train_algo, pz_algo, train_config_dict, estim_config_dict)
+
+
+def test_dnf_ENF():
+    train_config_dict = {'zmin': 0.0, 'zmax': 3.0, 'nzbins': 301,
+                         'hdf5_groupname': 'photometry',
+                         'model': 'model.tmp'}
+    estim_config_dict = {'zmin': 0.0, 'zmax': 3.0, 'nzbins': 301,
+                         'hdf5_groupname': 'photometry',
+                         'min_n': 15, 'bad_redshift_val': 99.,
+                         'bad_redshift_err': 10.,
+                         'model': 'model.tmp',
+                         'nondetect_replace': True,
+                         'selection_mode': 0}
+
+    train_algo = dnf.DNFInformer
+    pz_algo = dnf.DNFEstimator
+    results, rerun_results, _ = one_algo("DNF_ENF", train_algo, pz_algo, train_config_dict, estim_config_dict)
+
+
+def test_dnf_DNF():
+    train_config_dict = {'zmin': 0.0, 'zmax': 3.0, 'nzbins': 301,
+                         'hdf5_groupname': 'photometry',
+                         'model': 'model.tmp'}
+    estim_config_dict = {'zmin': 0.0, 'zmax': 3.0, 'nzbins': 301,
+                         'hdf5_groupname': 'photometry',
+                         'min_n': 15, 'bad_redshift_val': 99.,
+                         'bad_redshift_err': 10.,
+                         'model': 'model.tmp',
+                         'nondetect_replace': True,
+                         'selection_mode': 2}
+
+    train_algo = dnf.DNFInformer
+    pz_algo = dnf.DNFEstimator
+    results, rerun_results, _ = one_algo("DNF_DNF", train_algo, pz_algo, train_config_dict, estim_config_dict)
+
+
+def test_dnf_badval():
+    train_config_dict = {'zmin': 0.0, 'zmax': 3.0, 'nzbins': 301,
+                         'hdf5_groupname': 'photometry',
+                         'model': 'model.tmp'}
+    estim_config_dict = {'zmin': 0.0, 'zmax': 3.0, 'nzbins': 301,
+                         'hdf5_groupname': 'photometry',
+                         'min_n': 15, 'bad_redshift_val': 99.,
+                         'bad_redshift_err': 10.,
+                         'model': 'model.tmp',
+                         'nondetect_replace': True,
+                         'selection_mode': "BADVAL"}
+
+    train_algo = dnf.DNFInformer
+    pz_algo = dnf.DNFEstimator
+    with pytest.raises(TypeError):
+        results, rerun_results, _ = one_algo("DNF_DNF", train_algo, pz_algo, train_config_dict, estim_config_dict)
+
+
+def test_dnf_badint():
+    train_config_dict = {'zmin': 0.0, 'zmax': 3.0, 'nzbins': 301,
+                         'hdf5_groupname': 'photometry',
+                         'model': 'model.tmp'}
+    estim_config_dict = {'zmin': 0.0, 'zmax': 3.0, 'nzbins': 301,
+                         'hdf5_groupname': 'photometry',
+                         'min_n': 15, 'bad_redshift_val': 99.,
+                         'bad_redshift_err': 10.,
+                         'model': 'model.tmp',
+                         'nondetect_replace': True,
+                         'selection_mode': 99}
+
+    train_algo = dnf.DNFInformer
+    pz_algo = dnf.DNFEstimator
+    with pytest.raises(ValueError):
+        results, rerun_results, _ = one_algo("DNF_DNF", train_algo, pz_algo, train_config_dict, estim_config_dict)
